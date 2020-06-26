deea27e3ffbc79a728b34e9175d9c07f1112cf50
chayadeaver <chayadeaver@gmail.com>
2020-06-26 00:48:48 -0400

    implemented routes, navbar, and jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Home } from './components/Home'
+import { NoMatch } from './components/NoMatch'
+import Login from './components/Login'
+import { Layout } from './components/Layout'
+import { NavBar } from './components/NavBar'
+import { Jumbotron} from './components/Jumbotron'
 
 class App extends React.Component {
   render () {
     return (
-      <div className="App">
-        App
-      </div>
+      <React.Fragment>
+        <NavBar />
+        <Jumbotron />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route component={NoMatch} />
+
+              {/* <Route path="/signup" component={Signup} /> */}
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
     );
   }
   
