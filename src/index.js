889be04aabc23420809692d1b07445fc62a64cc1
chayadeaver <chayadeaver@gmail.com>
2020-06-25 21:29:15 -0400

    moved store out of index.js into store.jsx

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import usersReducer from './reducers/users'
+import store from './store'
 
-const reducer = combineReducers({
-    users: usersReducer
-})
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
     <Provider store={store}>
