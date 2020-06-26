1abc7aae066375cff318bec45ab3dc371cf48ccd
chayadeaver <chayadeaver@gmail.com>
2020-06-25 21:21:00 -0400

    setup redux store with an empty state

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import usersReducer from './reducers/users'
 
+const reducer = combineReducers({
+    users: usersReducer
+})
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
 
