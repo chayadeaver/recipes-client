003e9a1426095bef0b3b60a6650e985d401db477
chayadeaver <chayadeaver@gmail.com>
2020-06-26 22:28:06 -0400

    add currentUser and loginform

--- src/store.jsx
+++ src/store.jsx
@@ -1,9 +1,17 @@
+// Redux store
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import usersReducer from './reducers/users'
+import currentUser from './reducers/currentUser'
+import loginForm from './reducers/loginForm'
+
+
 import thunk from 'redux-thunk'
 
+// displaying different syntax options
 const reducer = combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    currentUser,
+    loginForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
