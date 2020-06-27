59c44f3584a2cdbf2920726425501ca1f9669060
chayadeaver <chayadeaver@gmail.com>
2020-06-26 22:29:17 -0400

    added form and event handlers

---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { updateLoginForm } from '../actions/loginForm'
+import { login } from '../actions/currentUser'
+
+
+const Login = ({ loginFormData, updateLoginForm, login }) => {
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        const updatedFormInfo = {
+            ...loginFormData,
+            [name]: value
+        }
+        updateLoginForm(updatedFormInfo)
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        login(loginFormData)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Email" value={loginFormData.email} name="email" onChange={handleInputChange} />
+            <input type="password" placeholder="Password" value={loginFormData.password} name="password" onChange={handleInputChange} />
+            <input type="submit" value="Login"/>
+        </form>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        loginFormData: state.loginForm
+    }
+}
+
+export default connect(mapStateToProps, { updateLoginForm, login })(Login)
