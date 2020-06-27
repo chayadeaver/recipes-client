420b6e40edfa5b5652c5251e63a30120d17d8450
chayadeaver <chayadeaver@gmail.com>
2020-06-27 17:38:07 -0400

    Simplied navbar for now

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,51 @@
 import React from 'react'
-import { Nav, Navbar} from 'react-bootstrap'
-import styled from 'styled-components'
+// import { Nav, Navbar} from 'react-bootstrap'
+// import styled from 'styled-components'
+import { connect } from 'react-redux'
+import Login from './Login'
+import Logout from './Logout'
+
 // import { getCurrentUser } from '../actions/currentUser'
 
-const Styles = styled.div`
-    .navbar {
-        background-color: #222;
-    }
-    .navbar-brand, .navbar-nav .nav-link {
-        color: #bbb;
+// const Styles = styled.div`
+//     .navbar {
+//         background-color: #222;
+//     }
+//     .navbar-brand, .navbar-nav .nav-link {
+//         color: #bbb;
 
-        &:hover {
-            color: white;
-        }
-    }
-`;
+//         &:hover {
+//             color: white;
+//         }
+//     }
+// `;
 
-export const NavBar = (props) => (
-    <Styles>
-        <Navbar expand="lg" fixed="top">
-            <Navbar.Brand href="/">MyFamilyRecipes</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/logout">Logout</Nav.Link></Nav.Item> : <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </Styles>
-)
+const NavBar = ({ currentUser }) => {
+
+    return (
+        <div className="NavBar">
+            { currentUser ? <p>Welcome, {currentUser.name}</p> : ""}
+            { currentUser ? <Logout /> : <Login /> }
+        </div>
+    )
+    // <Styles>
+    //     <Navbar expand="lg" fixed="top">
+    //         <Navbar.Brand href="/">MyFamilyRecipes</Navbar.Brand>
+    //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //         <Navbar.Collapse id="basic-navbar-nav">
+    //             <Nav className="ml-auto">
+    //                 <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+    //                 <Nav.Item><Nav.Link href="/logout">Logout</Nav.Link></Nav.Item> : <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
+    //             </Nav>
+    //         </Navbar.Collapse>
+    //     </Navbar>
+    // </Styles>
+}
+
+const mapStateToProps = ({ currentUser }) => {
+    return {
+        currentUser
+    }
+}
+
+export default connect(mapStateToProps)(NavBar)
