1056a2e0fe28b8ab5e0a91429f88c3a5c0998de8
chayadeaver <chayadeaver@gmail.com>
2020-06-27 13:51:51 -0400

    added CLEAR_CURRENT_USER case

--- src/reducers/currentUser.jsx
+++ src/reducers/currentUser.jsx
@@ -3,6 +3,8 @@ export default (state = null, action) => {
 
         case "SET_CURRENT_USER":
             return action.user
+        case "CLEAR_CURRENT_USER":
+            return null
         default:
             return state
     }
