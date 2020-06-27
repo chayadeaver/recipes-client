74a20577b11211fbf12725d7444a28c52c157759
chayadeaver <chayadeaver@gmail.com>
2020-06-26 22:28:31 -0400

    set up synchronous actions

---
+++ src/actions/currentUser.jsx
@@ -0,0 +1,50 @@
+// synchronous action creators
+export const setCurrentUser = user => {
+    return {
+        type: "SET_CURRENT_USER",
+        user
+    }
+}
+
+export const login = credentials => {
+    console.log("credentials are", credentials)
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(res => res.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+// asynchronous action creators
+export const getCurrentUser = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/get_current_user", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(res => res.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
+}
