83602c8a08823e10103ead75e55f77bbf3a07d7f
chayadeaver <chayadeaver@gmail.com>
2020-06-27 17:37:35 -0400

    Got rid of react router code for now

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Home } from './components/Home'
-import { NoMatch } from './components/NoMatch'
-import Login from './components/Login'
-import Logout from './components/Logout'
+import './App.css'
+import NavBar  from './components/NavBar'
 // import { Layout } from './components/Layout'
-// import { NavBar } from './components/NavBar'
 // import { Jumbotron} from './components/Jumbotron'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
+import MainContainer from './components/MainContainer';
 
 class App extends React.Component {
   
@@ -18,33 +15,18 @@ class App extends React.Component {
 
   render () {
     return (
-      this.props.currentUser ? <Logout /> : <Login />
-      // <React.Fragment>
-      //   <NavBar />
-      //   <Jumbotron />
-        
-      //   <Layout>
-      //     <Router>
-      //       <Switch>
-      //         <Route exact path="/" component={Home} />
-      //         <Route path="/logout" component={Logout} />
-      //         <Route path="/login" component={Login} />
-      //         <Route component={NoMatch} />
-              
-      //         {/* <Route path="/signup" component={Signup} /> */}
-      //       </Switch>
-      //     </Router>
-      //   </Layout>
-      // </React.Fragment>
+      <div className="App">
+          <MainContainer />
+          <NavBar />
+          
+          {/* <Jumbotron /> */}
+          
+          {/* <Layout /> */}
+      </div>
     );
   }
   
 }
 
-const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser
-  }
-}
 
-export default connect(mapStateToProps, { getCurrentUser })(App);
+export default connect(null, { getCurrentUser })(App);
