e6bebd1aa9c595bb20ad5159caace11ed7384837
chayadeaver <chayadeaver@gmail.com>
2020-06-27 17:38:52 -0400

    added main container and added jumbotron

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Jumbotron} from './Jumbotron'
+import { Layout } from './Layout'
+
+const MainContainer = () => {
+    return (
+        <div className="MainContainer">
+            <Jumbotron />
+            <Layout />
+        </div>
+    )
+}
+
+export default MainContainer
