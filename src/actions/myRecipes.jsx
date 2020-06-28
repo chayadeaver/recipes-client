2a4aaf558232350d9890ebab4f53aea7cad33373
chayadeaver <chayadeaver@gmail.com>
2020-06-28 15:18:43 -0400

    added async action getMyRecipes

--- src/actions/myRecipes.jsx
+++ src/actions/myRecipes.jsx
@@ -1,6 +1,30 @@
+// synchronus actions
 export const setMyRecipes = recipes => {
     return {
         type: "SET_MY_RECIPES",
         recipes
+    }
+}
+
+// asynchronus actions
+
+export const getMyRecipes = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/current_user_recipes", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(res => res.json())
+        .then(response => {
+            if (response.error) {
+                alert(response.error)
+            } else {
+                dispatch(setMyRecipes([]))
+            }
+        })
+        .catch(console.log)
     }
 }
