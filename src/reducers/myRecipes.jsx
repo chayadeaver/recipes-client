b446a094b6439c30dace0d1cc46b85e9f01d16a7
chayadeaver <chayadeaver@gmail.com>
2020-06-28 15:57:21 -0400

    changes state in redux store

--- src/reducers/myRecipes.jsx
+++ src/reducers/myRecipes.jsx
@@ -1,5 +1,7 @@
 export default (state = [], action) => {
     switch(action.type) {
+        case "SET_MY_RECIPES":
+            return action.recipes
         default:
             return state
     }
