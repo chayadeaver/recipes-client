9504d97be8d8566d46dc6b40006cb22f697e9513
chayadeaver <chayadeaver@gmail.com>
2020-06-28 23:09:12 -0400

    added myRecipe component

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Jumbotron} from './Jumbotron'
 import { Layout } from './Layout'
+import MyRecipes from './MyRecipes'
 
 const MainContainer = () => {
     return (
         <div className="MainContainer">
             <Jumbotron />
             <Layout />
+            <MyRecipes />
         </div>
     )
 }
