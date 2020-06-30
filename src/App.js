34747899c6b9ce1a180d9e574b9710bab4b1900d
chayadeaver <chayadeaver@gmail.com>
2020-06-29 22:02:46 -0400

    added signup route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css'
 import NavBar  from './components/NavBar'
 import Login  from './components/Login'
-import Logout  from './components/Logout'
+import Signup  from './components/Signup'
 import MyRecipes  from './components/MyRecipes'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
@@ -20,6 +20,7 @@ class App extends React.Component {
       <div className="App">
         <NavBar />
         <MainContainer />
+          <Route exact path="/signup" component={Signup} />
           <Route exact path="/login" component={Login} />
           <Route exact path ="/my-recipes" component={MyRecipes}/>
       </div>
