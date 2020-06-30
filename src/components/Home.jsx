e288e5c44184a038105931c3215acb2ab1c3fa6c
chayadeaver <chayadeaver@gmail.com>
2020-06-30 12:47:04 -0400

    added links to signup and login components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Login from './Login'
+import Signup from './Signup'
+import { Link } from 'react-router-dom'
 
-export const Home = () => {
+
+
+const Home = () => {
     return (
         <div>
             <h2>Browse Recipes</h2>
+            <h4>To add recipes, please <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link></h4>
         </div>
     )
 }
 
+export default Home
