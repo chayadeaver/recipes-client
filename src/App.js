6c276c9346f268a3aa0aad790dbba6d826cc91a7
chayadeaver <chayadeaver@gmail.com>
2020-06-30 12:31:06 -0400

    added props for loggedIn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css'
 import NavBar  from './components/NavBar'
+import Home  from './components/Home'
 import Login  from './components/Login'
+import Logout  from './components/Logout'
 import Signup  from './components/Signup'
 import MyRecipes  from './components/MyRecipes'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
 import MainContainer from './components/MainContainer';
-import { Route } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 
 class App extends React.Component {
   
@@ -16,18 +18,30 @@ class App extends React.Component {
   }
 
   render () {
+    const { loggedIn } = this.props
+    debugger
     return (
       <div className="App">
         <NavBar />
+        { loggedIn ? <Logout history={this.props.history}/> : null}
         <MainContainer />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path ="/my-recipes" component={MyRecipes}/>
+          <Switch>
+            <Route exact path="/" render={(props) => loggedIn ? <MyRecipes {...props}/> : <Home {...props}/>} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/logout" render={(props) => <Logout {...props}/>} />
+            <Route exact path ="/my-recipes" component={MyRecipes}/>
+          </Switch>
       </div>
     );
   }
   
 }
 
+const mapStateToProps = state => {
+  return {
+    loggedIn: !!state.currentUser
+  }
+}
 
-export default connect(null, { getCurrentUser })(App);
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
