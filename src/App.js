b771c13094b67a4bd444df0b8a2b44291a976fc0
chayadeaver <chayadeaver@gmail.com>
2020-06-30 20:20:24 -0400

    removed debugger and logout route

--- src/App.js
+++ src/App.js
@@ -19,17 +19,15 @@ class App extends React.Component {
 
   render () {
     const { loggedIn } = this.props
-    debugger
     return (
       <div className="App">
         <NavBar />
-        { loggedIn ? <Logout history={this.props.history}/> : null}
+        { loggedIn ? <Logout /> : null}
         <MainContainer />
           <Switch>
             <Route exact path="/" render={(props) => loggedIn ? <MyRecipes {...props}/> : <Home {...props}/>} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/logout" render={(props) => <Logout {...props}/>} />
             <Route exact path ="/my-recipes" component={MyRecipes}/>
           </Switch>
       </div>
