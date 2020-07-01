3862891b33f456f1ba9be170281b2702323bf98d
chayadeaver <chayadeaver@gmail.com>
2020-06-30 20:21:50 -0400

    changed logout form to button

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -3,13 +3,16 @@ import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser'
 
 
-const Logout = ({ logout, history }) => {
+const Logout = ({ logout }) => {
+
+    const handleLogout = () => {
+        logout()
+    }
 
     return (
-        <form onSubmit={() => logout(history)}>
-            <input type="submit" value="Logout"/>
-        </form>
+        <button onClick={handleLogout}>Logout</button>
     )
 }
+
 
 export default connect(null, { logout })(Logout)
