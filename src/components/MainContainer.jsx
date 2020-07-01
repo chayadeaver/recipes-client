c683e86f7f0b7a5d70e8e5985a523097e020a146
chayadeaver <chayadeaver@gmail.com>
2020-06-30 20:22:12 -0400

    removed MyRecipe Component

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Jumbotron} from './Jumbotron'
 import { Layout } from './Layout'
-import MyRecipes from './MyRecipes'
 
 const MainContainer = () => {
     return (
         <div className="MainContainer">
             <Jumbotron />
             <Layout />
-            <MyRecipes />
         </div>
     )
 }
