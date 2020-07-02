a367502686c70ae15a9e8f03c3ad182aa7692d56
chayadeaver <chayadeaver@gmail.com>
2020-07-02 16:30:09 -0400

    add CLEAR_RECIPES

--- src/reducers/myRecipes.jsx
+++ src/reducers/myRecipes.jsx
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch(action.type) {
         case "SET_MY_RECIPES":
             return action.recipes
+        case "CLEAR_RECIPES":
+            return []
         default:
             return state
     }
