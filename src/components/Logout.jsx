334818d9e895159d53121ef00965de02d6474efa
chayadeaver <chayadeaver@gmail.com>
2020-07-02 16:29:19 -0400

    added redirect to /

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser'
+import { withRouter } from 'react-router-dom'
 
 
-const Logout = ({ logout }) => {
-
-    const handleLogout = () => {
-        logout()
-    }
+const Logout = ({ logout, history }) => {
 
     return (
-        <button onClick={handleLogout}>Logout</button>
+        <form onSubmit={(event) => {
+                event.preventDefault()
+                logout()
+                history.push('/')
+            }
+        }>
+            <input type="submit" value="Log Out" />
+        </form>
     )
 }
 
 
-export default connect(null, { logout })(Logout)
+export default withRouter(connect(null, { logout })(Logout))
