1842a271d041b79ef5a123e2e182f88b9d8cee3f
chayadeaver <chayadeaver@gmail.com>
2020-07-02 16:29:37 -0400

    added links to navbar if logged in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 // import { Nav, Navbar} from 'react-bootstrap'
 // import styled from 'styled-components'
+// import Login from './Login'
+import Logout from './Logout'
+import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 // import { getCurrentUser } from '../actions/currentUser'
@@ -18,11 +21,13 @@ import { connect } from 'react-redux'
 //     }
 // `;
 
-const NavBar = ({ currentUser }) => {
+const NavBar = ({ currentUser, loggedIn }) => {
 
     return (
         <div className="NavBar">
-            { currentUser ? <p>Welcome, {currentUser.attributes.name}</p> : ""}
+            <NavLink exact activeClassName="active" to="/myrecipes">|  My Recipes  |  </NavLink>
+            <NavLink exact activeClassName="active" to="/recipes/new">|  New Recipe  |  </NavLink>
+            { loggedIn ? <Logout /> : null}
         </div>
     )
     // <Styles>
@@ -41,7 +46,8 @@ const NavBar = ({ currentUser }) => {
 
 const mapStateToProps = ({ currentUser }) => {
     return {
-        currentUser
+        currentUser,
+        loggedIn: !!currentUser
     }
 }
 
