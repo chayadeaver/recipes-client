0d002deb8b432ad209d7e9fe5393cf14f9bfb8ee
chayadeaver <chayadeaver@gmail.com>
2020-07-02 16:28:22 -0400

    added a br tag

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,7 @@ const Home = () => {
         <div>
             <h2>Browse Recipes</h2>
             <h4>To add recipes, please <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link></h4>
+            <br />
         </div>
     )
 }
