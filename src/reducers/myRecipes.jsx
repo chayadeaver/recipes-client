ae83b795708f5bc0fe6826f59466caef469954b0
chayadeaver <chayadeaver@gmail.com>
2020-07-03 13:37:00 -0400

    add new case for ADD_RECIPE

--- src/reducers/myRecipes.jsx
+++ src/reducers/myRecipes.jsx
@@ -1,9 +1,13 @@
-export default (state = [], action) => {
+const initialState = []
+
+export default (state = initialState, action) => {
     switch(action.type) {
         case "SET_MY_RECIPES":
             return action.recipes
+        case "ADD_RECIPE":
+            return state.recipes.concat(action.recipes)
         case "CLEAR_RECIPES":
-            return []
+            return initialState
         default:
             return state
     }
