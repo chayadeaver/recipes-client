62c80def41598bd646110b7b9dd02ec230f1733f
chayadeaver <chayadeaver@gmail.com>
2020-07-04 14:26:28 -0400

    set up add_ingredient action

--- src/actions/newRecipeForm.jsx
+++ src/actions/newRecipeForm.jsx
@@ -1,9 +1,23 @@
 // synchronous actions
 
-export const updateNewRecipeForm = (name, value) => {
-    console.log(name, value)
+export const updateNewRecipeForm = formData => {
     return {
         type: "UPDATE_NEW_RECIPE_FORM",
-        formData: { name, value }
+        formData
     }
 }
+export const addIngredient = ingredient => {
+    return {
+        type: "ADD_INGREDIENT",
+        ingredient
+    }
+}
+
+// export const updateIngredients = (name, value) => {
+//     const formData = { name, value }
+//     console.log("formData in the action creator", formData)
+//     return {
+//         type: "UPDATE_INGREDIENTS",
+//         formData
+//     }
+// }
