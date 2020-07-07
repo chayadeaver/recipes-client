751f4570fcfde2a9c3619cbc04fd8ff5c1e7d812
chayadeaver <chayadeaver@gmail.com>
2020-07-06 23:14:44 -0400

    adding conditional

--- src/components/containers/MyRecipes.jsx
+++ src/components/containers/MyRecipes.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import RecipeCard from '../RecipeCard'
+import { getMyRecipes } from '../../actions/myRecipes'
 import { connect } from 'react-redux'
 
 
 
 const MyRecipes = ({ myRecipes }) => {
     const recipeCards = myRecipes.map(r => <RecipeCard recipe={r} key={r.id} />)
-    return (
-        recipeCards
-    )
+
+    useEffect(() => {
+        getMyRecipes()
+        
+    }, [])
+    
+    return myRecipes.length === 0 ? recipeCards : <div><h5>There are no recipes yet</h5></div>
+    
 }
 
 const mapStateToProps = ({ myRecipes }) => {
