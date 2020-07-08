2d53030bbaa466882369c991f162b1cbb0dfe586
chayadeaver <chayadeaver@gmail.com>
2020-07-08 11:43:04 -0400

    added link and home route

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ import RecipeForm  from './components/containers/RecipeForm'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
 import MainContainer from './components/containers/MainContainer';
-import { Route, Switch, withRouter } from 'react-router-dom'
+import { Route, Switch, withRouter, Link } from 'react-router-dom'
+import { getAllRecipes } from './actions/myRecipes';
 
 class App extends React.Component {
   
   componentDidMount() {
     this.props.getCurrentUser()
+    this.props.getAllRecipes()
   }
 
   render () {
@@ -25,24 +27,27 @@ class App extends React.Component {
     return (
       <div className="App">
         <MainContainer />
-        { loggedIn ? <NavBar location={this.props.location}/> : <Home /> }
+        { loggedIn ? <NavBar location={this.props.location}/> : <h2>To add recipes, please <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link></h2> }
+          
           <Switch>
             <Route exact path="/signup" render={({ history }) => <Signup history={ history }/>} />
             <Route exact path="/login" component={Login} />
             <Route exact path ="/myrecipes" component={MyRecipes}/>
             <Route exact path ="/recipes/new" component={RecipeForm}/>
             <Route exact path ="/recipes/:id" render={props => {
-                const recipe = recipes.find(recipe => recipe.id === props.match.params.id)
+                const recipe = this.props.allRecipes.find(recipe => recipe.id === props.match.params.id)
                 console.log(recipe)
                 return <RecipeCard recipe={recipe} {...props}/>
               }
             }/>
             <Route exact path ="/recipes/:id/edit" render={props => {
-                const recipe = recipes.find(recipe => recipe.id === props.match.params.id)
+                const recipe = this.props.allRecipes.find(recipe => recipe.id === props.match.params.id)
                 console.log(recipe)
                 return <RecipeForm recipe={recipe} {...props}/>
               }
             }/>
+            <Route exact path="/" component={Home} />
+
           </Switch>
       </div>
     );
@@ -53,8 +58,9 @@ class App extends React.Component {
 const mapStateToProps = state => {
   return {
     loggedIn: !!state.currentUser,
-    recipes: state.myRecipes
+    // recipes: state.myRecipes,
+    allRecipes: state.myRecipes.allRecipes
   }
 }
 
-export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
+export default withRouter(connect(mapStateToProps, { getCurrentUser, getAllRecipes })(App));
