caa5c621bf4c1de4a781ca25345c2026ad812015
chayadeaver <chayadeaver@gmail.com>
2020-07-07 21:13:34 -0400

    removed hooks for now

--- src/components/containers/MyRecipes.jsx
+++ src/components/containers/MyRecipes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import RecipeCard from '../RecipeCard'
-import { getMyRecipes } from '../../actions/myRecipes'
+// import { getMyRecipes } from '../../actions/myRecipes'
 import { connect } from 'react-redux'
 
 
@@ -8,12 +8,15 @@ import { connect } from 'react-redux'
 const MyRecipes = ({ myRecipes }) => {
     const recipeCards = myRecipes.map(r => <RecipeCard recipe={r} key={r.id} />)
 
-    useEffect(() => {
-        getMyRecipes()
+    // useEffect(() => {
+    //     getMyRecipes()
         
-    }, [])
+    // }, [])
     
-    return myRecipes.length === 0 ? recipeCards : <div><h5>There are no recipes yet</h5></div>
+    return (
+        // myRecipes.length === 0 ? recipeCards : <div><h5>There are no recipes yet</h5></div>
+        recipeCards
+        )
     
 }
 
