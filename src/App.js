eed32e66cfa8cd975cecb6572788a923b2cb996a
chayadeaver <chayadeaver@gmail.com>
2020-07-08 20:48:11 -0400

    added NoMatch route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import './App.scss'
 import './App.css'
 import NavBar  from './components/NavBar'
 import Home  from './components/Home'
 import RecipeCard  from './components/RecipeCard'
 import Login  from './components/sessions/Login'
-// import Logout  from './components/sessions/Logout'
+import NoMatch  from './components/NoMatch'
 import Signup  from './components/sessions/Signup'
 import MyRecipes  from './components/containers/MyRecipes'
 import RecipeForm  from './components/containers/RecipeForm'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
 import MainContainer from './components/containers/MainContainer';
-import { Route, Switch, withRouter, Link } from 'react-router-dom'
+import { Route, Switch, withRouter, Link, Redirect } from 'react-router-dom'
 import { getAllRecipes } from './actions/myRecipes';
 
 class App extends React.Component {
@@ -23,7 +24,7 @@ class App extends React.Component {
 
   render () {
 
-    const { loggedIn, recipes } = this.props
+    const { loggedIn, allRecipes } = this.props
     return (
       <div className="App">
         <MainContainer />
@@ -32,21 +33,23 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/signup" render={({ history }) => <Signup history={ history }/>} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/no-match" component={NoMatch} />
             <Route exact path ="/myrecipes" component={MyRecipes}/>
             <Route exact path ="/recipes/new" component={RecipeForm}/>
             <Route exact path ="/recipes/:id" render={props => {
-                const recipe = this.props.allRecipes.find(recipe => recipe.id === props.match.params.id)
+                const recipe = allRecipes.find(recipe => recipe.id === props.match.params.id)
                 console.log(recipe)
                 return <RecipeCard recipe={recipe} {...props}/>
               }
             }/>
             <Route exact path ="/recipes/:id/edit" render={props => {
-                const recipe = this.props.allRecipes.find(recipe => recipe.id === props.match.params.id)
+                const recipe = allRecipes.find(recipe => recipe.id === props.match.params.id)
                 console.log(recipe)
                 return <RecipeForm recipe={recipe} {...props}/>
               }
             }/>
             <Route exact path="/" component={Home} />
+            <Redirect to="/no-match" />
 
           </Switch>
       </div>
