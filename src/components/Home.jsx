75d11fa3ae723a02b8ed950878930b25f3890aed
chayadeaver <chayadeaver@gmail.com>
2020-07-11 01:44:50 -0400

    added search bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,20 @@ import React from 'react'
 // import Signup from './Signup'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-
+import SearchBar from './SearchBar';
 
 const Home = props => {
-    const recipeCards = props.recipes.length > 0 ? 
-        props.recipes.map((r, i) => (<div key={i}>
+    const getRecipes = () => {
+        let { recipes } = props
+        if (props.searchResults !== undefined && props.searchResults.length > 0) {
+            recipes = props.searchResults
+            return recipes
+        } else {
+            return recipes
+        }
+    }
+    const recipeCards = getRecipes().length > 0 ? 
+        getRecipes().map((r, i) => (<div key={i}>
             <Link key={r.id} to={`/recipes/${r.id}`}>{r.attributes.name}</Link>
         </div>)) : <p>This is myRecipes with an empty array of recipes</p>
 
@@ -16,6 +25,7 @@ const Home = props => {
         <div>
             <h4>Browse Recipes</h4>
             <br />
+            <SearchBar searchBar={props.searchBar} location={props.location}/>
             {recipeCards}
         </div>
     )
@@ -23,7 +33,8 @@ const Home = props => {
 
 const mapStateToProps = state => {
     return {
-        recipes: state.myRecipes.allRecipes
+        recipes: state.myRecipes.allRecipes,
+        searchResults: state.myRecipes.searchResults
     }
     
 }
