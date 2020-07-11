96d84a1f03bb6519f8c716aa6fb4b7ebc0ce1887
chayadeaver <chayadeaver@gmail.com>
2020-07-11 01:45:00 -0400

    search form

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getAllRecipeSearchResults } from '../reducers/myRecipes'
+
+
+const SearchBar = props => {
+    const [ query, setQuery ] = useState("")
+    const dispatch = useDispatch()
+    const allRecipes = useSelector(state => state.myRecipes.allRecipes,
+            
+    )
+    // debugger
+    const handleChange = e => {
+        setQuery(e.target.value)
+        dispatch(getAllRecipeSearchResults(query, allRecipes)) 
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={(e) => e.preventDefault()}>
+                <div className="form-group">
+                <input autoFocus onChange={handleChange} type="search" id="searchBar" placeholder="Search" />
+                <button type="submit" className="btn btn-success btn-sm">Search</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SearchBar
