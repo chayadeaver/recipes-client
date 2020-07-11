5d52453b2f13448d088f51c5a6bd939ecc7fb7f4
chayadeaver <chayadeaver@gmail.com>
2020-07-10 21:26:08 -0400

    deleted comments

--- src/reducers/myRecipes.jsx
+++ src/reducers/myRecipes.jsx
@@ -5,18 +5,6 @@ const initialState = {
     userRecipes: []
 }
 
-// export default (state = initialState, action) => {
-//     switch(action.type) {
-//         case "SET_MY_RECIPES":
-//             return action.recipes
-//         case "ADD_RECIPE":
-//             return state.concat(action.recipe)
-//         case "CLEAR_RECIPES":
-//             return initialState
-//         default:
-//             return state
-//     }
-// }
 
 export default (state = initialState, action) => {
     switch(action.type) {
