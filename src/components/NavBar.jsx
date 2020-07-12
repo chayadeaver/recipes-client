b0567fbd2e689287d9049006ad8eb91bfe409ddb
chayadeaver <chayadeaver@gmail.com>
2020-07-12 16:28:52 -0400

    Styled NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,48 +1,71 @@
 import React from 'react'
-// import { Nav, Navbar} from 'react-bootstrap'
-// import styled from 'styled-components'
+import { Nav, Navbar} from 'react-bootstrap'
+import styled from 'styled-components'
 // import Login from './Login'
-import Logout from './sessions/Logout'
+// import Logout from './sessions/Logout'
+import { logout } from '../actions/currentUser'
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 // import { getCurrentUser } from '../actions/currentUser'
 
-// const Styles = styled.div`
-//     .navbar {
-//         background-color: #222;
-//     }
-//     .navbar-brand, .navbar-nav .nav-link {
-//         color: #bbb;
+const Styles = styled.div`
+    .navbar {
+        background-color: #222;
+    }
+    .navbar-brand, .navbar-nav .nav-link {
+        color: #bbb;
 
-//         &:hover {
-//             color: white;
-//         }
-//     }
-// `;
+        &:hover {
+            color: white;
+        }
+    }
+`;
 
-const NavBar = ({ currentUser, loggedIn }) => {
+const NavBar = ({ currentUser, loggedIn, logout }) => {
 
+    // return (
+    //     <div className="NavBar">
+    //         <NavLink exact activeClassName="active" to="/">|  Home  |  </NavLink>
+    //         <NavLink exact activeClassName="active" to="/myrecipes">|  My Recipes  |  </NavLink>
+    //         <NavLink exact activeClassName="active" to="/recipes/new">|  New Recipe  |  </NavLink>
+    //         { loggedIn ? <><p id="loggedin">Logged in as {currentUser.attributes.name}</p><Logout /></> : null}
+    //     </div>
+    // )
+    const renderNavbar = () => {
+        if (loggedIn) {
+            return (
+                <>
+                <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/myrecipes">My Recipes</Nav.Link></Nav.Item> 
+                <Nav.Item><Nav.Link href="/recipes/new">New Recipe</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link onClick={logout}>Logout</Nav.Link></Nav.Item>
+                </>
+            )
+    } else {
+        return (
+            <>
+            <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="/signup">Sign Up</Nav.Link></Nav.Item> 
+            <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
+            </>
+        )
+        
+    }}
     return (
-        <div className="NavBar">
-            <NavLink exact activeClassName="active" to="/">|  Home  |  </NavLink>
-            <NavLink exact activeClassName="active" to="/myrecipes">|  My Recipes  |  </NavLink>
-            <NavLink exact activeClassName="active" to="/recipes/new">|  New Recipe  |  </NavLink>
-            { loggedIn ? <><p id="loggedin">Logged in as {currentUser.attributes.name}</p><Logout /></> : null}
-        </div>
+        <Styles>
+        <Navbar expand="lg" fixed="top">
+            <Navbar.Brand href="/">MyFamilyRecipes</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    {renderNavbar()}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </Styles>
     )
-    // <Styles>
-    //     <Navbar expand="lg" fixed="top">
-    //         <Navbar.Brand href="/">MyFamilyRecipes</Navbar.Brand>
-    //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //         <Navbar.Collapse id="basic-navbar-nav">
-    //             <Nav className="ml-auto">
-    //                 <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-    //                 <Nav.Item><Nav.Link href="/logout">Logout</Nav.Link></Nav.Item> : <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
-    //             </Nav>
-    //         </Navbar.Collapse>
-    //     </Navbar>
-    // </Styles>
+    
 }
 
 const mapStateToProps = ({ currentUser }) => {
@@ -52,4 +75,4 @@ const mapStateToProps = ({ currentUser }) => {
     }
 }
 
-export default connect(mapStateToProps)(NavBar)
+export default connect(mapStateToProps, { logout })(NavBar)
