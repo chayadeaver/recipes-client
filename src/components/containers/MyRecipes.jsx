4fc8b5e8d945c1258341282407dc5d259cb58f3c
chayadeaver <chayadeaver@gmail.com>
2020-07-13 11:48:35 -0400

    filtered through recipes to user's

--- src/components/containers/MyRecipes.jsx
+++ src/components/containers/MyRecipes.jsx
@@ -4,11 +4,25 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const MyRecipes = props => {
-    const recipeCards = props.recipes.length > 0 ? 
-        props.recipes.map((r, i) => (<div key={i}>
-            <Link key={r.id} to={`/recipes/${r.id}`}>{r.name}</Link>
-        </div>
-            )) : <p>This is myRecipes with an empty array of recipes</p>
+    
+    // useEffect(() => {
+    //     getMyRecipes()
+    // })
+    // debugger
+    const userRecipes = props.recipes.filter(recipe => {
+        // debugger
+        return recipe.attributes.user_id == props.currentUser.id
+    })
+    // debugger
+    const recipeCards = userRecipes.length > 0 ? 
+        userRecipes.map((r, i) => {
+            // debugger
+            return (
+                <div key={i}>
+                    <Link key={r.id} to={`/recipes/${r.id}`}>{r.attributes.name}</Link>
+                </div>
+            )}): <p>This is myRecipes with an empty array of recipes</p>
+    
     
     return (
         recipeCards
@@ -16,9 +30,10 @@ const MyRecipes = props => {
     
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = ({ myRecipes, currentUser }) => {
     return {
-        recipes: state.myRecipes.userRecipes
+        recipes: myRecipes.allRecipes,
+        currentUser
     }
 }
 
