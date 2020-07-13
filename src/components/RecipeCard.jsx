ff0c4fdd21fe3ef2b285f3665f454068fad008df
chayadeaver <chayadeaver@gmail.com>
2020-07-12 23:32:49 -0400

    added logic to render form if not signed in

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -4,39 +4,59 @@ import { connect } from 'react-redux'
 import { getRecipe } from '../actions/myRecipes'
 
 
-const RecipeCard = ({ recipe, currentUser, getRecipe, match }) => {
+const RecipeCard = ({ recipe, currentUser, getRecipe, match, loggedIn }) => {
     
     useEffect(() => {
         getRecipe(match.params.id)
     })
 
     
-        if (recipe){
+        if (recipe && loggedIn ){
             const recipeUserId = recipe.relationships.user.data.id
             const currentUserId = currentUser.id
 
             return (
                 <div>
-            <h4>{recipe.attributes.name}</h4>
-            <div>
-                <img src={recipe.attributes.image_url} alt="_blank"/>
-            </div>
-            <p>{recipe.attributes.description}</p>
-            <br />
-            <h5>Ingredients:</h5>
-            {recipe.attributes.ingredients.map((ing, idx) => {
-                return (
-                    <p key={idx}>
-                        {ing.quantity} {ing.unit} {ing.name}
-                    </p>
-                )
-            })}
-            <h5>Instructions:</h5>
-            <p>{recipe.attributes.instructions}</p>
-            {recipeUserId === currentUserId ? <Link to={`/recipes/${recipe.id}/edit`}>Edit This Recipe</Link> : null}
+                    <h4>{recipe.attributes.name}</h4>
+                    <div>
+                        <img src={recipe.attributes.image_url} alt="_blank"/>
+                    </div>
+                    <p>{recipe.attributes.description}</p>
+                    <br />
+                    <h5>Ingredients:</h5>
+                    {recipe.attributes.ingredients.map((ing, idx) => {
+                        return (
+                            <p key={idx}>
+                                {ing.quantity} {ing.unit} {ing.name}
+                            </p>
+                        )
+                    })}
+                    <h5>Instructions:</h5>
+                    <p>{recipe.attributes.instructions}</p>
+                    {recipeUserId === currentUserId ? <Link to={`/recipes/${recipe.id}/edit`}>Edit This Recipe</Link> : null}
                 </div>
             )
-            }
+            } else if (recipe){
+                return (
+                    <div>
+                        <h4>{recipe.attributes.name}</h4>
+                        <div>
+                            <img src={recipe.attributes.image_url} alt="_blank"/>
+                        </div>
+                        <p>{recipe.attributes.description}</p>
+                        <br />
+                        <h5>Ingredients:</h5>
+                        {recipe.attributes.ingredients.map((ing, idx) => {
+                            return (
+                                <p key={idx}>
+                                    {ing.quantity} {ing.unit} {ing.name}
+                                </p>
+                            )
+                        })}
+                        <h5>Instructions:</h5>
+                        <p>{recipe.attributes.instructions}</p>
+                    </div>
+            )}
         return <p>This is a recipe card with no recipe</p>
         
     
@@ -45,6 +65,7 @@ const RecipeCard = ({ recipe, currentUser, getRecipe, match }) => {
 const mapStateToProps = state => {
     return {
         currentUser: state.currentUser,
+        loggedIn: !!state.currentUser,
         recipe: state.myRecipes.recipe
     }
 }
