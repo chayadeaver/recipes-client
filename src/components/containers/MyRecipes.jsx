45018d81dfff7bfb831d0976ffdc1497fc42cc58
chayadeaver <chayadeaver@gmail.com>
2020-10-08 14:32:10 -0400

    add card

--- src/components/containers/MyRecipes.jsx
+++ src/components/containers/MyRecipes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { Card, Button } from "react-bootstrap"
 
 const MyRecipes = ({ recipes, currentUser }) => {
     const userRecipes = recipes.filter(recipe => {
@@ -9,13 +10,27 @@ const MyRecipes = ({ recipes, currentUser }) => {
     const recipeCards = userRecipes.length > 0 ? 
         userRecipes.map((r, i) => {
             return (
-                <div key={i}>
-                    <Link key={r.id} to={`/recipes/${r.id}`}>{r.attributes.name}</Link>
-                </div>
+                <>
+                    {/* <Link key={r.id} to={`/recipes/${r.id}`}>{r.attributes.name}</Link> */}
+                    <Card className="recipe-card" key={i}>
+                        <div className="imageContainer">
+                        <Card.Img variant="top" src={r.attributes.image_url} />
+                        </div>
+                        
+                <Card.Body>
+                    <Card.Title>{r.attributes.name}</Card.Title>
+                    <Card.Text>{r.attributes.description}</Card.Text>
+                    <Button variant="primary" href={`/recipes/${r.id}`}>View Recipe</Button>
+                </Card.Body>
+            </Card>
+                </>
             )}): <p>This is myRecipes with an empty array of recipes</p>
     
     return (
-        recipeCards
+        <div className="recipe-container">
+                {recipeCards}
+        </div>
+        
     )
     
 }
