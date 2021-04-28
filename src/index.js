10db8ab21c523c9b457712b255325242d86814d4
chayadeaver <chayadeaver@gmail.com>
2021-04-27 21:57:44 -0400

    add persistor to persist state

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
-import store from './store'
+import { store, persistor} from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss'
 import { BrowserRouter as Router} from 'react-router-dom'
@@ -13,7 +14,9 @@ import { BrowserRouter as Router} from 'react-router-dom'
 ReactDOM.render(
     <Provider store={store}>
         <Router>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </Router>
     </Provider>, 
 document.getElementById('root'));
