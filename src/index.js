a54c6d04d7e3364da78a927350d3bffc6e2a1a51
chayadeaver <chayadeaver@gmail.com>
2021-04-27 22:48:19 -0400

    comment out import

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
-import { store, persistor} from './redux/store'
-import { PersistGate } from 'redux-persist/integration/react';
+import { store} from './redux/store'
+// import { PersistGate } from 'redux-persist/integration/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss'
 import { BrowserRouter as Router} from 'react-router-dom'
